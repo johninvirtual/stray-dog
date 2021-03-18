39c637cf5017988feddb408bc251c29260dcc1c8
John Marshal <john.marshal@smartenovations.com>
2021-03-18 20:10:09 +0530

    Add quote and test github actions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Stray Dog - Community</h1>
+      <h3>A community for stray dogs. Homo-Sapiens can also participate (after all, why should we show racism to dogs). Our unity to MOON 🚀</h3>
       <img src={logo} alt="logo"/>
     </div>
   );
